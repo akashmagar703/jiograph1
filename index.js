bec0ea74274769f0b66a3a36082562d86579bbc2
Akash Satish Magar <57744831+akashmagar703@users.noreply.github.com>
2024-04-04 15:21:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function about(){
+    let about = document.getElementById('element1')
+    about.innerText = "Our unique matching system lets you find just the tour you want for your next holiday. Our tour managers are qualified, skilled, and friendly to bring you the best service."
+}
+function about1(){
+    let about = document.getElementById('element2')
+   
+    about.innerText = "We offer a wide variety of personally picked tours with destinations all over the globe. Our team offers only the best selection of affordable and luxury hotels to our clients."
+}
